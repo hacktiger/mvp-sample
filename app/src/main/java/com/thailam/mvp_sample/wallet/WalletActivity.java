e566d499df4eeee0c99d3bb3c13189ad46cfca1f
ngthailam <ngthailam98@gmail.com>
2019-03-29 09:19:11 +0700

    Change tab space

--- app/src/main/java/com/thailam/mvp_sample/wallet/WalletActivity.java
+++ app/src/main/java/com/thailam/mvp_sample/wallet/WalletActivity.java
@@ -21,7 +21,7 @@ public class WalletActivity extends AppCompatActivity implements WalletContract.
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// init view components test test
+		// init view components test test s sc s sc s s cc
 		initButtons();
 		initTxtView();
 		initProgressBar();
