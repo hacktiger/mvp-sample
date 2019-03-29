8e0135c02140466c52cbb0c781313cb60d1ee6b2
hacktiger <hacktiegr1989@gmail.com>
2019-03-29 10:12:34 +0700

    test the indent in windows

--- app/src/main/java/com/thailam/mvp_sample/wallet/WalletActivity.java
+++ app/src/main/java/com/thailam/mvp_sample/wallet/WalletActivity.java
@@ -13,63 +13,63 @@ import com.thailam.mvp_sample.R;
 import java.util.List;
 
 public class WalletActivity extends AppCompatActivity implements WalletContract.View {
-	private WalletContract.Presenter mWalletPresenter;
-	private ProgressBar mProgressBar;
-	private TextView mTxt1, mTxt2;
+    private WalletContract.Presenter mWalletPresenter;
+    private ProgressBar mProgressBar;
+    private TextView mTxt1, mTxt2;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// init view components test test s sc s sc s s cc
-		initButtons();
-		initTxtView();
-		initProgressBar();
-		initPresenter();
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // test the new indent style
+        initButtons();
+        initTxtView();
+        initProgressBar();
+        initPresenter();
+    }
 
-	@Override
-	public void setProgressBar() {
-		mProgressBar.setVisibility(View.INVISIBLE);
-	}
+    @Override
+    public void setProgressBar() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
 
-	@Override
-	public void showWallets(List<Wallet> list) {
-		mTxt1.setText(list.get(1).mName);
-		mTxt2.setText(list.get(2).mName);
-	}
+    @Override
+    public void showWallets(List<Wallet> list) {
+        mTxt1.setText(list.get(1).mName);
+        mTxt2.setText(list.get(2).mName);
+    }
 
-	@Override
-	public Context getContext() {
-		return getApplicationContext();
-	}
+    @Override
+    public Context getContext() {
+        return getApplicationContext();
+    }
 
-	private void initPresenter() {
-		if (mWalletPresenter == null) {
-			mWalletPresenter = new WalletPresenter(this);
-			mWalletPresenter.initRepository();
-		}
-	}
+    private void initPresenter() {
+        if (mWalletPresenter == null) {
+            mWalletPresenter = new WalletPresenter(this);
+            mWalletPresenter.initRepository();
+        }
+    }
 
-	private void initButtons() {
-		Button loadBtn = findViewById(R.id.btn_show);
-		loadBtn.setOnClickListener(v -> {
-			mWalletPresenter.loadData();
-		});
-		Button addBtn = findViewById(R.id.btn_add);
-		addBtn.setOnClickListener(v -> {
-			mWalletPresenter.addData(new Wallet(27, "wallet1", 100)); // Dummy data
-		});
-	}
+    private void initButtons() {
+        Button loadBtn = findViewById(R.id.btn_show);
+        loadBtn.setOnClickListener(v -> {
+            mWalletPresenter.loadData();
+        });
+        Button addBtn = findViewById(R.id.btn_add);
+        addBtn.setOnClickListener(v -> {
+            mWalletPresenter.addData(new Wallet(27, "wallet1", 100)); // Dummy data
+        });
+    }
 
-	private void initTxtView() {
-		mTxt1 = findViewById(R.id.text_1);
-		mTxt2 = findViewById(R.id.text_2);
-	}
+    private void initTxtView() {
+        mTxt1 = findViewById(R.id.text_1);
+        mTxt2 = findViewById(R.id.text_2);
+    }
 
-	private void initProgressBar() {
-		mProgressBar = findViewById(R.id.progress_bar);
-		mProgressBar.setIndeterminate(true);
-		mProgressBar.setVisibility(View.VISIBLE);
-	}
+    private void initProgressBar() {
+        mProgressBar = findViewById(R.id.progress_bar);
+        mProgressBar.setIndeterminate(true);
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
 }
